25abc85d546aad6f3518822dd44332d950f30b87
ruziev-dev <ruziev.dev@gmail.com>
2022-12-11 18:14:56 +0300

    separate main module to  utils module

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,7 @@
 #include <SPIFFS.h>
 #include <ArduinoJson.h>
 #include "Display.h"
+#include "utils.h"
 
 #define JSON_CONFIG_FILE "/test_config.json"
 
@@ -28,7 +29,8 @@ const long timeoutTime = 2000;
 
 void initWiFi()
 {
-  WiFi.mode(WIFI_STA);
+  // WiFi.mode(WIFI_STA);
+  WiFi.mode(WIFI_AP_STA);
   WiFi.begin(ssid, password);
   Serial.print("Connecting to WiFi ..");
   while (WiFi.status() != WL_CONNECTED)
@@ -37,33 +39,6 @@ void initWiFi()
     delay(1000);
   }
   Serial.println(WiFi.localIP());
-}
-
-String SendHTML(String data)
-{
-  String ptr = "<!DOCTYPE html> <html>\n";
-  ptr += "<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">\n";
-  ptr += "<title>LED Control</title>\n";
-  ptr += "<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}\n";
-  ptr += "body{margin-top: 50px;} h1 {color: #444444;margin: 50px auto 30px;} h3 {color: #444444;margin-bottom: 50px;}\n";
-  ptr += ".button {display: block;width: 80px;background-color: #3498db;border: none;color: white;padding: 13px 30px;text-decoration: none;font-size: 25px;margin: 0px auto 35px;cursor: pointer;border-radius: 4px;}\n";
-  ptr += ".button-on {background-color: #3498db;}\n";
-  ptr += ".button-on:active {background-color: #2980b9;}\n";
-  ptr += ".button-off {background-color: #34495e;}\n";
-  ptr += ".button-off:active {background-color: #2c3e50;}\n";
-  ptr += "p {font-size: 14px;color: #888;margin-bottom: 10px;}\n";
-  ptr += "</style>\n";
-  ptr += "</head>\n";
-  ptr += "<body>\n";
-  ptr += "<h1>ESP32 Web Server</h1>\n";
-  ptr += "<h3>Using Access Point(AP) Mode</h3>\n";
-  ptr += data;
-  ptr += "<a class=\"button button-on\" href=\"/led1on\">ON</a>";
-  ptr += "<a class=\"button button-off\" href=\"/led1on\">OFF</a>";
-
-  ptr += "</body>\n";
-  ptr += "</html>\n";
-  return ptr;
 }
 
 void handle_OnConnect(AsyncWebServerRequest *request)
@@ -86,6 +61,14 @@ void handle_NotFound(AsyncWebServerRequest *request)
   request->send(404, "text/plain", "{\"error\": \"Error 404\"}");
 }
 
+void check_auth(AsyncWebServerRequest *request)
+{
+  //if (!request->authenticate(www_username, www_password))
+  //  request->requestAuthentication();
+
+  request->send(404, "text/plain", "{\"error\": \"Error 404\"}");
+}
+
 void setup()
 {
 
@@ -96,6 +79,7 @@ void setup()
   display.init();
   display.setStateInfo(WiFi.localIP().toString(), WiFi.macAddress());
 
+  server.on("/auth", check_auth);
   server.on("/", handle_OnConnect);
   server.on("/info", handle_info);
   server.onNotFound(handle_NotFound);
@@ -103,6 +87,16 @@ void setup()
   server.begin();
   Serial.println("HTTP server started");
   Serial.println("MD5: " + ESP.getSketchMD5());
+
+  Serial.print(F("Inizializing FS..."));
+  if (SPIFFS.begin())
+  {
+    Serial.println(F("done."));
+  }
+  else
+  {
+    Serial.println(F("fail."));
+  }
 }
 
 void loop()
---
+++ src/utils.h
@@ -0,0 +1,8 @@
+#include <Arduino.h>
+
+
+#ifndef utils_h
+#define utils_h
+#endif
+
+String SendHTML(String data);
